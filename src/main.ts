e05d71040265ee034a229d25f63b2956ed52db93
diaznicolasandres1 <nico@Nicolass-MacBook-Pro.local>
2025-03-13 22:57:50 -0300

    Swagger docu

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 API corriendo en: http://localhost:3000`);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("API de Recetas")
+    .setDescription("Documentación de la API para gestionar recetas y usuarios")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, document);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}`);
+  console.log(`📄 Swagger disponible en: http://localhost:${PORT}/api`);
 }
 bootstrap();
